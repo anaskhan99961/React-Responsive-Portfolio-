924630c3f310b7f5c28d9d3f61d07c50613f87a4
Muhammad Anas Khan <anaskhan99961@gmail.com>
2026-03-16 05:53:54 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,8 +14,8 @@ function Contact({ onOpenModal }) {
         </button>
         <div className="social-links">
           <a href="https://github.com/anaskhan99961" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://www.linkedin.com/in/muhammad-anas-khan-817954249/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="http://linkedin.com/in/anas-khan-817954249" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="http://linkedin.com/in/anas-khan-817954249" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://x.com/anaskha84631891?s=21" target="_blank" rel="noopener noreferrer">Twitter</a>
         </div>
       </div>
     </section>
